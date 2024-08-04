9eb51647334f885d46144d6744b408368b4833df
Noorain-Raza-coder <noorain2raza@gmail.com>
2024-08-05 00:37:14 +0530

    add , remove and display products list's functions are created.

--- backend/mysqlConn.py
+++ backend/mysqlConn.py
@@ -2,8 +2,13 @@ import mysql.connector
 
 
 def mysqlConnection():
+    ## create connection
+    cnn = mysql.connector.connect(user = 'root' , password = 'Mysql$2002' , host = '127.0.0.1' , database = 'grocery_store')
 
-    cnn = mysql.connector.connect(host = '127.0.0.1' , user = 'root' , password = 'Mysql$2002' , database = 'grocery_store')
+    try :
+        if cnn :
+            return cnn
+    except Exception as e:
+        print(e)
 
-    return cnn
 
